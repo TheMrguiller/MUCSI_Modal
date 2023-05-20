89c358f8fb0db902905a2475ac8c62e5d8bdbb33
Lander San Millan <50888543+landersanmi@users.noreply.github.com>
2023-05-20 17:32:18 +0200

    feat: 1st version of flamingo-megatiny pretrained from scratch
    Coco2017 Dataset

--- flamingo-megatiny/training/train.sh
+++ flamingo-megatiny/training/train.sh
@@ -1,25 +1,26 @@
 #!/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=2,3
-NUM_GPU=2
+export CUDA_VISIBLE_DEVICES=0
+NUM_GPU=1
 
 ARGS="
 --output_dir ./flamingo-coco
---run_name flamingo-tiny-vitL
+--run_name flamingo-megatiny-vitL-coco
 --do_train --do_eval
 --optim adamw_torch
 --learning_rate 0.0001 
---warmup_steps 5000
+--warmup_steps 500
 --lr_scheduler_type constant_with_warmup
---per_device_train_batch_size 8
---per_device_eval_batch_size 64
+--per_device_train_batch_size 16
+--per_device_eval_batch_size 16
 --gradient_accumulation_steps 1
 --evaluation_strategy steps
---eval_steps 1000
+--eval_steps 500
+--num_train_epochs 3
 --save_strategy epoch
 --save_total_limit 2
 --log_level info
---dataloader_num_workers 8
+--dataloader_num_workers 14
 --dataloader_pin_memory True
 --fp16
 --report_to wandb
